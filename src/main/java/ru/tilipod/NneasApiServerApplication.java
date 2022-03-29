8d85a7e15f7733f17c2cb5c82cd79afca51064ad
Tilipod <Tilipod@mail.ru>
2022-03-29 19:18:36 +0300

    Init API-server for 2NEAS-1

---
+++ src/main/java/ru/tilipod/NneasApiServerApplication.java
@@ -0,0 +1,12 @@
+package ru.tilipod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NneasApiServerApplication {
+
+    public static void main(String[] args) {
+            SpringApplication.run(NneasApiServerApplication.class, args);
+        }
+}
